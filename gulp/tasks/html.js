f48a625c3eb29c5c87d133d66c9efe12446b67f7
lianet <jack.tolmak@yandex.ru>
2024-12-08 19:37:09 +0300

    commit

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -31,7 +31,7 @@ export const html = () => {
         )
         .pipe(fileinclude())
         .pipe(app.plugins.replace(/@img\//g, "img/"))
-        .pipe(app.plugins.if(app.isBuild, webpHtmlNosvg()))
+        // .pipe(app.plugins.if(app.isBuild, webpHtmlNosvg()))
         .pipe(
             app.plugins.if(
                 hasVersion,
--- src/js/app.js
+++ src/js/app.js
@@ -4,23 +4,7 @@
 $(function () {
 
 
-    // detect webP support
-    function testWebP(callback) {
-        let webP = new Image();
-        webP.onload = webP.onerror = function () {
-            callback(webP.height == 2);
-        };
-        webP.src =
-            "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-    }
-
-    testWebP(function (support) {
-        if (support == true) {
-            $("body").addClass("webp");
-        } else {
-            $("body").addClass("no-webp");
-        }
-    });
+    // detect user device
 
     const isMobile = {
         Android: function () {
